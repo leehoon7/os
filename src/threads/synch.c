c861bdbe9510b19cdb98d7d5a53263fe154681b1
leehoon <leehoon@kaist.ac.kr>
2019-10-04 02:29:15 +0900

    adsf

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -412,7 +412,6 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
 
   if (!list_empty (&cond->waiters))
   {
-    list_sort (&cond->waiters, priority_compare_sema, NULL);
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
   }
