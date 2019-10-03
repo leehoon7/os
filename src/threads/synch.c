9ccc4ca1b34298747d54a7cd675906d20ac87a2f
leehoon <leehoon@kaist.ac.kr>
2019-10-04 03:14:12 +0900

    adsf

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -126,6 +126,7 @@ sema_up (struct semaphore *sema)
   struct thread *t;
 
   if (!list_empty (&sema->waiters)){
+    list_sort (&sema->waiters, priority_compare_s, NULL);
     t = list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem);
     thread_unblock (t);
