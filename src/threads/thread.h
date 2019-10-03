7059e97bfe285a10bdbcd992588302b59f91c0d3
leehoon <leehoon@kaist.ac.kr>
2019-10-03 23:17:50 +0900

    adsf

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -94,6 +94,7 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+    struct list holding_lock;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
