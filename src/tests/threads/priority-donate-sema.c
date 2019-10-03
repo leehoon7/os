7b216af8623dfca65ec7eeb4ba59591617c6b77a
leehoon <leehoon@kaist.ac.kr>
2019-10-04 01:58:54 +0900

    adsf

--- src/tests/threads/priority-donate-sema.c
+++ src/tests/threads/priority-donate-sema.c
@@ -16,7 +16,7 @@
 #include "threads/synch.h"
 #include "threads/thread.h"
 
-struct lock_and_sema 
+struct lock_and_sema
   {
     struct lock lock;
     struct semaphore sema;
@@ -27,7 +27,7 @@ static thread_func m_thread_func;
 static thread_func h_thread_func;
 
 void
-test_priority_donate_sema (void) 
+test_priority_donate_sema (void)
 {
   struct lock_and_sema ls;
 
@@ -42,13 +42,15 @@ test_priority_donate_sema (void)
   thread_create ("low", PRI_DEFAULT + 1, l_thread_func, &ls);
   thread_create ("med", PRI_DEFAULT + 3, m_thread_func, &ls);
   thread_create ("high", PRI_DEFAULT + 5, h_thread_func, &ls);
+  msg ("sema up in main.");
   sema_up (&ls.sema);
   msg ("Main thread finished.");
 }
 
 static void
-l_thread_func (void *ls_) 
+l_thread_func (void *ls_)
 {
+  msg ("Thread L start..");
   struct lock_and_sema *ls = ls_;
 
   lock_acquire (&ls->lock);
@@ -60,8 +62,9 @@ l_thread_func (void *ls_)
 }
 
 static void
-m_thread_func (void *ls_) 
+m_thread_func (void *ls_)
 {
+  msg ("Thread M start..");
   struct lock_and_sema *ls = ls_;
 
   sema_down (&ls->sema);
@@ -69,8 +72,9 @@ m_thread_func (void *ls_)
 }
 
 static void
-h_thread_func (void *ls_) 
+h_thread_func (void *ls_)
 {
+  msg ("Thread H start..");
   struct lock_and_sema *ls = ls_;
 
   lock_acquire (&ls->lock);
