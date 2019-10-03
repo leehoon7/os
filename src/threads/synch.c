56f555add6b63392d25b1b4a0c53adf3174f36bd
leehoon <leehoon@kaist.ac.kr>
2019-10-04 01:24:48 +0900

    adsf

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -370,7 +370,9 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
 
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  list_insert_ordered (&cond->waiters, &waiter->elem, priority_compare_s, NULL);
+  //list_push_back (&cond->waiters, &waiter.elem);
+  //list_insert_ordered (&sema->waiters, &thread_current ()->elem, priority_compare_s, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
